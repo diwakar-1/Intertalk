3ebc0114387154fbf616492eaba413ece51dfade
diwakar-1 <cutepika360@gmail.com>
2025-10-27 16:41:20 +0530

    Updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from 'react'
+import Link from "next/link"
+import Image from 'next/image'
 
 const Rootlayout = ({children}: {children:ReactNode}) => {
   return (
-    <div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex items-centre gap-2'>
+           <Image src="/logo3.svg" alt='logo' width={38} height={32}></Image>
+            <h2  className='text-primary-100'>INERTALK</h2>
+        </Link>
+      </nav>
       {children}
     </div>
   )
---
+++ app/api/vapi/generate/rout.ts
@@ -0,0 +1,3 @@
+export async function GET(){
+    return Response.json({ success: true, data: "THANK YOu!"},{status: 200});
+}
